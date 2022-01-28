34a836a8e91249f248d27fbffe8c2e9dcc84f94f
Drewbreezy <drewvdwest@yahoo.com>
2022-01-27 22:34:20 -0500

    Home Blog and Footer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+function originalDisplay() {
+  document.getElementById("section-blog_content_first").style.display =
+    "inline-block";
+}
+
 function readMore() {
   const dots = document.getElementById("dots");
   const moreText = document.getElementById("more");
@@ -40,4 +45,156 @@ function navMenu() {
 
 function submitNewsletter() {
   document.querySelector(".subscription").classList.add("done");
+}
+
+function blogSwitchFirst() {
+  const firstBlog = document.getElementById("section-blog_content_first");
+  const secondBlog = document.getElementById("section-blog_content_second");
+  const thirdBlog = document.getElementById("section-blog_content_third");
+  const fourthBlog = document.getElementById("section-blog_content_fourth");
+  const firstTitle = document.getElementById("section-blog_title_first");
+  const secondTitle = document.getElementById("section-blog_title_second");
+  const thirdTitle = document.getElementById("section-blog_title_third");
+  const fourthTitle = document.getElementById("section-blog_title_fourth");
+  const firstList = document.getElementById("section-blog_list_first");
+  const secondList = document.getElementById("section-blog_list_second");
+  const thirdList = document.getElementById("section-blog_list_third");
+  const fourthList = document.getElementById("section-blog_list_fourth");
+
+  if (secondBlog.style.display === "inline-block") {
+    secondBlog.style.display = "none";
+    firstBlog.style.display = "inline-block";
+    secondTitle.style.display = "none";
+    firstTitle.style.display = "inline-block";
+    secondList.style.display = "inline-block";
+    firstList.style.display = "none";
+  } else if (thirdBlog.style.display === "inline-block") {
+    thirdBlog.style.display = "none";
+    firstBlog.style.display = "inline-block";
+    thirdTitle.style.display = "none";
+    firstTitle.style.display = "inline-block";
+    thirdList.style.display = "inline-block";
+    firstList.style.display = "none";
+  } else if (fourthBlog.style.display === "inline-block") {
+    fourthBlog.style.display = "none";
+    firstBlog.style.display = "inline-block";
+    fourthTitle.style.display = "none";
+    firstTitle.style.display = "inline-block";
+    fourthList.style.display = "inline-block";
+    firstList.style.display = "none";
+  }
+}
+
+function blogSwitchSecond() {
+  const firstBlog = document.getElementById("section-blog_content_first");
+  const secondBlog = document.getElementById("section-blog_content_second");
+  const thirdBlog = document.getElementById("section-blog_content_third");
+  const fourthBlog = document.getElementById("section-blog_content_fourth");
+  const firstTitle = document.getElementById("section-blog_title_first");
+  const secondTitle = document.getElementById("section-blog_title_second");
+  const thirdTitle = document.getElementById("section-blog_title_third");
+  const fourthTitle = document.getElementById("section-blog_title_fourth");
+  const firstList = document.getElementById("section-blog_list_first");
+  const secondList = document.getElementById("section-blog_list_second");
+  const thirdList = document.getElementById("section-blog_list_third");
+  const fourthList = document.getElementById("section-blog_list_fourth");
+
+  if (firstBlog.style.display === "inline-block") {
+    firstBlog.style.display = "none";
+    secondBlog.style.display = "inline-block";
+    firstTitle.style.display = "none";
+    secondTitle.style.display = "inline-block";
+    firstList.style.display = "inline-block";
+    secondList.style.display = "none";
+  } else if (thirdBlog.style.display === "inline-block") {
+    thirdBlog.style.display = "none";
+    secondBlog.style.display = "inline-block";
+    thirdTitle.style.display = "none";
+    secondTitle.style.display = "inline-block";
+    thirdList.style.display = "inline-block";
+    secondList.style.display = "none";
+  } else if (fourthBlog.style.display === "inline-block") {
+    fourthBlog.style.display = "none";
+    secondBlog.style.display = "inline-block";
+    fourthTitle.style.display = "none";
+    secondTitle.style.display = "inline-block";
+    fourthList.style.display = "inline-block";
+    secondList.style.display = "none";
+  }
+}
+
+function blogSwitchThird() {
+  const firstBlog = document.getElementById("section-blog_content_first");
+  const secondBlog = document.getElementById("section-blog_content_second");
+  const thirdBlog = document.getElementById("section-blog_content_third");
+  const fourthBlog = document.getElementById("section-blog_content_fourth");
+  const firstTitle = document.getElementById("section-blog_title_first");
+  const secondTitle = document.getElementById("section-blog_title_second");
+  const thirdTitle = document.getElementById("section-blog_title_third");
+  const fourthTitle = document.getElementById("section-blog_title_fourth");
+  const firstList = document.getElementById("section-blog_list_first");
+  const secondList = document.getElementById("section-blog_list_second");
+  const thirdList = document.getElementById("section-blog_list_third");
+  const fourthList = document.getElementById("section-blog_list_fourth");
+
+  if (firstBlog.style.display === "inline-block") {
+    firstBlog.style.display = "none";
+    thirdBlog.style.display = "inline-block";
+    firstTitle.style.display = "none";
+    thirdTitle.style.display = "inline-block";
+    firstList.style.display = "inline-block";
+    thirdList.style.display = "none";
+  } else if (secondBlog.style.display === "inline-block") {
+    secondBlog.style.display = "none";
+    thirdBlog.style.display = "inline-block";
+    secondTitle.style.display = "none";
+    thirdTitle.style.display = "inline-block";
+    secondList.style.display = "inline-block";
+    thirdList.style.display = "none";
+  } else if (fourthBlog.style.display === "inline-block") {
+    fourthBlog.style.display = "none";
+    thirdBlog.style.display = "inline-block";
+    fourthTitle.style.display = "none";
+    thirdTitle.style.display = "inline-block";
+    fourthList.style.display = "inline-block";
+    thirdList.style.display = "none";
+  }
+}
+
+function blogSwitchFourth() {
+  const firstBlog = document.getElementById("section-blog_content_first");
+  const secondBlog = document.getElementById("section-blog_content_second");
+  const thirdBlog = document.getElementById("section-blog_content_third");
+  const fourthBlog = document.getElementById("section-blog_content_fourth");
+  const firstTitle = document.getElementById("section-blog_title_first");
+  const secondTitle = document.getElementById("section-blog_title_second");
+  const thirdTitle = document.getElementById("section-blog_title_third");
+  const fourthTitle = document.getElementById("section-blog_title_fourth");
+  const firstList = document.getElementById("section-blog_list_first");
+  const secondList = document.getElementById("section-blog_list_second");
+  const thirdList = document.getElementById("section-blog_list_third");
+  const fourthList = document.getElementById("section-blog_list_fourth");
+
+  if (firstBlog.style.display === "inline-block") {
+    firstBlog.style.display = "none";
+    fourthBlog.style.display = "inline-block";
+    firstTitle.style.display = "none";
+    fourthTitle.style.display = "inline-block";
+    firstList.style.display = "inline-block";
+    fourthList.style.display = "none";
+  } else if (secondBlog.style.display === "inline-block") {
+    secondBlog.style.display = "none";
+    fourthBlog.style.display = "inline-block";
+    secondTitle.style.display = "none";
+    fourthTitle.style.display = "inline-block";
+    secondList.style.display = "inline-block";
+    fourthList.style.display = "none";
+  } else if (thirdBlog.style.display === "inline-block") {
+    thirdBlog.style.display = "none";
+    fourthBlog.style.display = "inline-block";
+    thirdTitle.style.display = "none";
+    fourthTitle.style.display = "inline-block";
+    thirdList.style.display = "inline-block";
+    fourthList.style.display = "none";
+  }
 }
